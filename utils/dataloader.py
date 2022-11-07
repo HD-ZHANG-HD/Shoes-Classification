4b1376578a4604a39ee4874274dded25f9e9a145
HD-ZHANG-HD <351253183@qq.com>
2022-11-06 16:02:21 -0800

    1

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -61,19 +61,16 @@ def get_loader(cfg):
 
 
 class test_dataset:
-    def __init__(self, image_root, gt_root, testsize):
+    def __init__(self, image_root, testsize, dir):
         self.testsize = testsize
         self.images = [
             image_root + f for f in os.listdir(image_root) if f.endswith('.jpg') or f.endswith('.png')]
-        self.gts = [
-            gt_root + f for f in os.listdir(gt_root) if f.endswith('.tif') or f.endswith('.png')]
-        self.images = sorted(self.images)
-        self.gts = sorted(self.gts)
+        self.cls = [dir(f.split(os.sep)[-2]) for f in os.listdir(image_root)
+                    if f.endswith('.jpg') or f.endswith('.png')]
         self.transform = transforms.Compose([
             transforms.Resize((self.testsize, self.testsize)),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-        self.gt_transform = transforms.ToTensor()
         self.size = len(self.images)
         self.index = 0
 
@@ -81,21 +78,13 @@ class test_dataset:
         image = self.rgb_loader(self.images[self.index])
         image = self.transform(image).unsqueeze(0)
 
-        gt = self.binary_loader(self.gts[self.index])
-
-        name = self.images[self.index].split('/')[-1]
-
-        image_for_post = self.rgb_loader(self.images[self.index])
-        image_for_post = image_for_post.resize(gt.size)
-
-        if name.endswith('.jpg'):
-            name = name.split('.jpg')[0] + '.png'
+        cls = self.cls[self.index]
 
         self.index += 1
         self.index = self.index % self.size
 
-        return image, gt, name, np.array(image_for_post)
-
+        return image, cls
+        
     def rgb_loader(self, path):
         with open(path, 'rb') as f:
             img = Image.open(f)
